3084260b5dabfcc3672a00e0c87ef651c1582cbd
Semen Khramtsov <hrams205@gmail.com>
2026-02-19 22:34:45 +0300

    add program.cs

--- Program.cs
+++ Program.cs
@@ -2,28 +2,26 @@
 
 Order order = new();
 
-// Заказываем Coffee с опциями и размером Large
 Drink coffee = new() { Name = "Coffee", Cost = 2.00m };
 coffee.AddOption(new Option { Name = "Milk", Cost = 0.50m });
 coffee.AddOption(new Option { Name = "Sugar", Cost = 0.20m });
 order.AddItem(new OrderItem(coffee, DrinkSize.Large));
 
-// Заказываем Tea с опцией и размером Small
 Drink tea = new() { Name = "Tea", Cost = 1.50m };
 tea.AddOption(new Option { Name = "Lemon", Cost = 0.30m });
 order.AddItem(new OrderItem(tea, DrinkSize.Small));
 
-// Допустим, добавим ещё пару напитков для проверки скидки:
 order.AddItem(new OrderItem(new Drink { Name = "Coffee", Cost = 2.00m }, DrinkSize.Medium));
 order.AddItem(new OrderItem(new Drink { Name = "Tea", Cost = 1.50m }, DrinkSize.Medium));
 
 decimal subtotal = order.GetSubtotal();
+
 var discountService = new DiscountService();
 decimal discount = discountService.CalculateTotalDiscount(order);
 decimal finalPrice = subtotal - discount;
 
-Console.WriteLine("Subtotal: " + subtotal);
-Console.WriteLine("Discount: " + discount);
-Console.WriteLine("Final Price: " + finalPrice);
-Console.ReadLine();
+var invoiceGenerator = new InvoiceGenerator();
+string invoice = invoiceGenerator.GenerateInvoice(order, finalPrice, discount);
+Console.WriteLine(invoice);
 
+Console.ReadLine();
